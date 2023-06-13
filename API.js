d49c4d9033e13df0badc5966fdb70de3c1ecbbc3
Ywatal <133248396+Ywatal@users.noreply.github.com>
2023-06-13 11:19:44 -0300

    Added database upload to Clarity, and standardized input from both APIs

--- API.js
+++ API.js
@@ -17,7 +17,13 @@ const db = mysql.createConnection({
     database: 'MRAPID',
 });
 
+/*
+Eventual data dictionary (Its a goddamn Hashmap, I hate Javascript), 
+probably to map the different units/paramters from the different API's
+to one unit/parameter that will go into the database.
+*/
 var data_dict = {
+    //OPENAQ
     "pm10µg/m³" : "pm10_mass_conc",
     "coppm" : "co_conc",
     "o3ppm" : "o3_conc",
@@ -26,6 +32,8 @@ var data_dict = {
     "no2ppm" : "no2_conc",
     "noppm" : "no_conc",
     "noxppm" : "nox_conc",
+
+    //CLARIty
 }
 
 //OPENAQ
@@ -51,6 +59,51 @@ async function get_loc_and_mes_AQ(){
           )
     } 
     catch (error) {
+        console.log(error.data);
+    }
+
+};
+
+async function OPENAQ_db_add(){
+    try {
+
+        //Make API request and create JSON Object
+        const URL = 'https://api.openaq.org/v2/latest?limit=100&page=1&offset=0&sort=desc&radius=1000&city=Detroit-Warren-Livonia&order_by=lastUpdated&dumpRaw=false';
+        const response = await got(URL);
+        const data = JSON.parse(response.body);
+
+        //Loop through all the locations in Detroit
+        let num_locations = data.meta.found;
+
+        for(let i = 0; i < num_locations; i++){
+
+            let lat = data.results[i].coordinates.latitude;
+            let long = data.results[i].coordinates.longitude;
+
+            //add measurements for each location in to table
+            for(let j = 0; j < data.results[i].measurements.length; j++){
+                let val = data.results[i].measurements[j].value;
+                let parameter = data.results[i].measurements[j].parameter;
+                let time = data.results[i].measurements[j].lastUpdated;
+                let unit = data.results[i].measurements[j].unit;
+                
+                //date = time.substring(0,10);
+            
+ 
+                db.query('INSERT INTO measurements (lati,longi,value,parameter,unit) VALUES (?,?,?,?,?)',
+                    [lat,long,val,parameter,unit],
+                    (err,result) => {
+                        if(err){
+                        console.log(err)
+                        }
+                    }
+                )
+
+            }
+
+        }
+    
+    } catch (error) {
         console.log(error.data);
     }
 
@@ -92,6 +145,63 @@ async function get_mes_Clarity(){
     }
 
 };
+
+async function CLARITY_db_add(){
+    try {
+        // in URL, use parameter "code" for Device ID and "datasourceId" for Datasource ID
+        const URL = 'https://clarity-data-api.clarity.io/v1/measurements?code=AT9BM6VV,ALQ1TJN6,AXPPQ0QF,AW2JHDG8&startTime=2023-06-01T00:00:00Z&endTime=2023-06-01T1:00:00Z';
+        const APIkey = 'WIISszA2VDYFNB37ZdpkHoX07UHIvPSBkxc2npSR';
+
+        const res = await fetch(URL, {
+            method: 'GET',
+            headers: {
+                'x-api-key': APIkey
+            }
+        });
+        const data = await res.json();
+
+        for(let i = 0; i < data.length; i++){
+            let lat = data[i].location.coordinates[0];
+            let long = data[i].location.coordinates[1];
+            let time = data[i].time; //didnt put this in yet becauce db's are finnicky with DATETIME formats
+
+            let add_mes = [[],[]];
+            add_mes.push([data[i].characteristics.relHumid.value,"Humidity","%"])
+            add_mes.push([data[i].characteristics.temperature.value,"Temperature","Celcius"])
+            add_mes.push([data[i].characteristics.no2Conc.calibratedValue,"no2","ppb"])
+
+            add_mes.push([data[i].characteristics.pm2_5ConcNum.value,"pm2.5","particles/cm3"])
+            add_mes.push([data[i].characteristics.pm2_5ConcMass.calibratedValue,"pm2.5","ug/m3"])
+            add_mes.push([data[i].characteristics.pm1ConcNum.value,"pm1","particles/cm3"])
+
+            add_mes.push([data[i].characteristics.pm1ConcMass.value,"pm1","ug/m3"])
+            add_mes.push([data[i].characteristics.pm10ConcNum.value,"pm10","particles/cm3"])
+            add_mes.push([data[i].characteristics.pm10ConcMass.calibratedValue,"pm10","ug/m3"])
+
+            for(let j = 0; j < add_mes.length; j ++){
+
+                if(add_mes[j][0]){
+
+                    db.query('INSERT INTO measurements (lati,longi,value,parameter,unit) VALUES (?,?,?,?,?)',
+                        [lat,long,add_mes[j][0],add_mes[j][1],add_mes[j][2]],
+                        (err,result) => {
+                            if(err){
+                            console.log(err)
+                            }
+                        }
+                    )
+
+                }
+
+            }
+
+        }
+
+    } catch (error) {
+        console.log(error.data);
+    }
+};
+
 
 //DST
 async function get_mes_DST(){
@@ -153,56 +263,8 @@ async function get_mes_DST(){
 })();
 */
 
-(async () => {
-    try {
 
-        //Make API request and create JSON Object
-        const URL = 'https://api.openaq.org/v2/latest?limit=100&page=1&offset=0&sort=desc&radius=1000&city=Detroit-Warren-Livonia&order_by=lastUpdated&dumpRaw=false';
-        const response = await got(URL);
-        const data = JSON.parse(response.body);
-
-        //Loop through all the locations in Detroit
-        let num_locations = data.meta.found;
-
-        for(let i = 0; i < num_locations; i++){
-
-            let lat = data.results[i].coordinates.latitude;
-            let long = data.results[i].coordinates.longitude;
-
-            //add measurements for each location in to table
-            for(let j = 0; j < data.results[i].measurements.length; j++){
-                let val = data.results[i].measurements[j].value;
-                let parameter = data.results[i].measurements[j].parameter;
-                let time = data.results[i].measurements[j].lastUpdated;
-                let unit = data.results[i].measurements[j].unit;
-                
-                //date = time.substring(0,10);
-            
- 
-                db.query('INSERT INTO measurements (lati,longi,value,parameter,unit) VALUES (?,?,?,?,?)',
-                    [lat,long,val,parameter,unit],
-                    (err,result) => {
-                        if(err){
-                        console.log(err)
-                        }
-                    }
-                )
-                
-                
-
-
-            }
-
-        }
-    
-    } catch (error) {
-        console.log(error.data);
-    }
-
-    
-
-
-})();
-
+OPENAQ_db_add();
+CLARITY_db_add();
 
 //ENDPAGE
