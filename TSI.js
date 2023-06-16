4169f4088d4924b215dde0bf1bf550f3f49fd3e6
Ywatal <133248396+Ywatal@users.noreply.github.com>
2023-06-16 14:59:08 -0300

    edited TSI.js to give devices data when run

--- TSI.js
+++ TSI.js
@@ -5,16 +5,11 @@ fetch("https://api-prd.tsilink.com/api/v3/external/oauth/client_credential/acces
   body: "client_id=fAGJGmQUCr3zENr7Asy7Q0YeJCXAYIzURkCHILJ2qz7KP90N&client_secret=cKSMDR9GXaKrfpI81xhSmAzgwUiC5CpR2m48dIVp8ad00GTvUtzZXN9BvpDtmsuy",
 })
   .then((response) => response.json())
-  .then((json) => console.log(json));
-
-
-// Get Devices 
-// fetch("https://api-prd.tsilink.com/api/v3/external/devices", {
-//   method: "GET",
-//   headers: {
-//     "Authorization": "Bearer KStzLMAsNwbAWk3Is3sJJ2DyvpsR", // "Authorization": "Bearer [access_token]",
-//     "Accept": "application/json",
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+    .then((json) => fetch("https://api-prd.tsilink.com/api/v3/external/devices", {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + json.access_token, // "Authorization": "Bearer [access_token]",
+        "Accept": "application/json",
+      },
+  }).then((response) => response.json())
+      .then((json) => console.log(json)));
