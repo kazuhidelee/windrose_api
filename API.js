1003ee725dbeb1d160ac605907a24bbf8abcf966
kristinemcl <kristinesoohyun@yahoo.com>
2023-07-11 18:08:21 -0400

    Checking if capitalization matters rip

--- API.js
+++ API.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
