341a2496eea9515b3f9c4a01e220b5d7d194fb71
Ywatal <133248396+Ywatal@users.noreply.github.com>
2023-07-17 15:17:28 -0300

    Delete purpleAir.js

--- purpleAir.js
+++
@@ -1,8 +0,0 @@
-//PurpleAIR information for requests to APIs as global VARS
-
-export const purpleAir_Monitors = ["WalledLakeSensor", "Pilgrim near Pine", "Birmingham", "Novi Township", "CBS News Detroit NEXT Weather",
-                            "AREN PROJECT Livonia", "JAM_OakPark", "Woodstock and Renfrew", "Crestwood High School - Courtyard", "Auddette/ Mayfair",
-                            "Timber Creek", "Appoline St", "Schafer Neighborhood", "Foley St",
-                            "EC-Deacon St", "EC_Clark", "EC-DHDC I", "La Salle Gardens (outside)", "Recovery Park Rooftop",
-                            "KatAir", "GCCS", "EC_Beiniteau 2", "EC-ECN Conner", "855 balfour",
-                            "SCSnet1", "Fraser MI", "Hilton", "Sterling Heights"];
